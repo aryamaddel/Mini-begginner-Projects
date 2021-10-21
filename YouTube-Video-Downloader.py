73aabdd88224c5720e348fc4361e1bddfac6f1fc
Arya Maddel <maddelarya@gmail.com>
2021-10-21 22:16:07 +0530

    change video download location

--- YouTube-Video-Downloader.py
+++ YouTube-Video-Downloader.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
-video = YouTube("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # Video URL
+video = YouTube("https://www.youtube.com/watch?v=RkC0l4iekYo")  # Video URL
 print(video.thumbnail_url)
 print(video.length)
 
@@ -8,6 +8,6 @@ print(video.title)
 print("downloading...")
 
 stream = video.streams.get_highest_resolution()
-stream.download(output_path='/Users/aryamaddel/Desktop')
+stream.download(output_path='/Users/aryamaddel/Downloads')
 
 print('Downloaded successfully!')
