2d84a944ea27564f2f5874f61801e51bda07fa99
Arya Maddel <maddelarya@gmail.com>
2021-12-18 02:11:39 +0530

    edit js

--- javascript/Tally counter/main.js
+++ javascript/Tally counter/main.js
@@ -1,6 +1,7 @@
-count = 0;
+let countEl = document.getElementById("count-el");
+let count = 0;
 function increment() {
   console.log("Button was clicked");
   count = count + 1;
-  document.getElementById("count-el").innerText = count;
+  countEl.innerText = count;
 }
