1ecb8c2e64bc7833759681195d652de3b7b8d3aa
Arya Maddel <maddelarya@gmail.com>
2021-12-26 01:35:21 +0530

    edit js

--- javascript/Tally counter/main.js
+++ javascript/Tally counter/main.js
@@ -1,18 +1,29 @@
 let countEl = document.getElementById("count-el");
 let readingEl = document.getElementById("reading-el");
 let count = 0;
-let readings = "readings: ";
+let readings = "readings:";
+let increment_status_checker = true;
 function increment() {
   console.log("Button was clicked");
   count = count + 1;
   countEl.innerText = count;
+  increment_status_checker = true;
 }
 function reset() {
   count = 0;
   countEl.innerText = count;
+  increment_status_checker = true;
 }
 function mark() {
-  console.log(count);
-  readings = readings + count + ", ";
-  readingEl.innerText = readings;
+  if (increment_status_checker == true) {
+    // console.log(count);
+    if (readings == "readings:") {
+      readings = readings + count;
+    } else {
+      readings = readings + ", " + count;
+    }
+
+    readingEl.innerText = readings;
+    increment_status_checker = false;
+  }
 }
