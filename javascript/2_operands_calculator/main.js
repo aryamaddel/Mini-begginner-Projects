5de8b0c27cf667441cd1d2ab16930018dfe391c5
Arya Maddel <maddelarya@gmail.com>
2022-01-12 22:10:28 +0530

    add JS

---
+++ javascript/2_operands_calculator/main.js
@@ -0,0 +1,13 @@
+let resultsEL = document.getElementById("results-el");
+function addition() {
+  resultsEL.innerText = 3 + 5;
+}
+function subtraction() {
+  resultsEL.innerText = 3 - 5;
+}
+function multiplication() {
+  resultsEL.innerText = 3 * 5;
+}
+function division() {
+  resultsEL.innerText = 3 / 5;
+}
