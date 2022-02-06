f095ec49c070e27b06615ae8a47bcb177421d16c
Arya Maddel <maddelarya@gmail.com>
2022-02-06 13:00:02 +0530

    Create password-generator.py

---
+++ python/password-generator.py
@@ -0,0 +1,22 @@
+import random
+import string
+characters = ''
+characters += string.ascii_letters + string.digits + string.punctuation
+
+# to add your own characters
+# characters += ''  
+
+def pass_geneator():
+    password = ''
+    for i in range(16):
+        password += random.choice(characters)
+    return password
+
+while True:
+    nav_input = input("To generate password type 'y'\nto exit type 'n'\n:")
+    if nav_input == 'y':
+        print("-----------------------------------\n")
+        print(pass_geneator() + '\n')  
+        print("-----------------------------------\n")
+    else:
+        quit()
