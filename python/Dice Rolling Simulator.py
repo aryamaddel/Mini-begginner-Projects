9a915e2e95820a9017434b2e2b90a4edc8fa1d7d
Arya Maddel <maddelarya@gmail.com>
2022-02-08 00:01:06 +0530

    created Dice Rolling Simulator.py and relocated mapit.py

---
+++ python/Dice Rolling Simulator.py
@@ -0,0 +1,9 @@
+import random
+
+def dice_roll():
+    number_of_dice = int(input("How many dice do you want to roll?\n:"))
+    for i in range(number_of_dice):
+        print('_________________________________________________________________\n')
+        print(random.randint(1, 6), '\n')
+
+dice_roll()
