a232036b25d5e9880857aa0ddc3e84d3e0d2d557
Arya Maddel <maddelarya@gmail.com>
2022-02-08 00:47:36 +0530

    Create Email Slicer.py

---
+++ python/Email Slicer.py
@@ -0,0 +1,12 @@
+# using regular expression module
+import re
+
+to_be_extracted = input('enter your data which you want to slice\n:')
+
+# "  [a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+  " 
+# This the regex which will help to pull out the email addresses
+emails = re.findall("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", to_be_extracted)
+
+
+for mail in emails:
+    print(mail)
