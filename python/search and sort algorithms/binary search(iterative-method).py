14916cac09ecf47b75ab9d3b43b9bc1628e0ee3c
Arya Maddel <maddelarya@gmail.com>
2022-02-11 00:02:52 +0530

    Update binary search(iterative-method).py

--- python/search and sort algorithms/binary search(iterative-method).py
+++ python/search and sort algorithms/binary search(iterative-method).py
@@ -1,4 +1,29 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]  # sorted list
+list = [12, 45, 67, 89, 99, 100]  # sorted list
 
-# numbner to search 6
-number = 1
+# numbner to search
+number = 100
+
+
+def search(list, n):
+    low = 0
+    high = len(list)-1
+    mid = 0
+
+    while low <= high:
+        mid = (low+high)//2
+        if list[mid] == n:
+            return mid
+        elif list[mid] < n:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+search_result = search(list, number)
+if search_result != -1:
+    print('Element found at : ', search_result)
+elif search_result == -1:
+    print("Element NOT Found")
+else:
+    print("Error")
