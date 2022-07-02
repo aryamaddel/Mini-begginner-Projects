a7c5e3469fbc0487d65517d127ee2e2176ad8e35
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2022-07-02 06:59:42 +0530

    Update Number Guessing Game(User).py

--- python/games/Number Guessing Game(User).py
+++ python/games/Number Guessing Game(User).py
@@ -1,21 +1,25 @@
 from random import randint
 
-upper = 20
-lower = 1
+
+def guess(x):
+    lower = 1
+    upper = x
+    feedback = ''
+
+    while feedback != 'c':
+        if lower != upper:
+            guess_number = randint(lower, upper)
+        else:
+            guess_number = lower
+        feedback = input(
+            f"\nIs the number {guess_number} Higher(h) OR Lower(l) OR Correct(c)").lower()
+        if feedback == 'h':
+            upper = guess_number - 1
+            continue
+        elif feedback == 'l':
+            lower = guess_number + 1
+            continue
+    print("YAY! I won")
 
 
-print("Think of any number between 1 and 20")
-
-while True:
-    guess_number = randint(lower, upper)
-    feedback = input(f"\nIs the number - {guess_number} Higher(H) or Lower(L) or Correct(C)").lower()
-
-    if feedback == "c":
-        print("\nYAY! I won\n")
-        exit()
-    elif feedback == 'h':
-        upper = guess_number-1
-        continue
-    elif feedback == 'l':
-        lower = guess_number+1
-        continue
+guess(20)
