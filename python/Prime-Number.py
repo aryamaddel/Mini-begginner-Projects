cda3c94458c0c9f814271712b106b7b806ae3d22
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2022-07-07 19:02:40 +0530

    Create Prime-Number.py

---
+++ python/Prime-Number.py
@@ -0,0 +1,13 @@
+def prime(n):
+    if n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+number = int(input("Enter a number: "))
+if prime(number):
+    print("The number is prime.")
+else:
+    print("The number is not prime.")
